9f078110d04c2d75fe73a6e73758efd244287848
saber <maysaber@126.com>
2019-07-16 22:19:47 +0800

    code clean, add iteration to crawl data

--- src/test/java/com/baidu/infra/services/AppTest.java
+++ src/test/java/com/baidu/infra/services/AppTest.java
@@ -11,55 +11,47 @@ import org.jsoup.nodes.*;
 
 import java.io.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void articalDetailShouldReturn200() throws FileNotFoundException, UnsupportedEncodingException {
+    public void getACCCCases() throws IOException, InterruptedException {
         String baseUrl = "https://www.accc.gov.au";
-        /*for (int i = 0; i < 344; i++) {
+        String paraPage = "";
+        int count = 0;
+        int totalPage = 344;
+        int timeInterval=5;
+
+        for (int i = 0; i < totalPage; i++) {
             if (i != 0)
-                baseUrl += "?page=" + i;
-        }*/
-        RestUtil.setBaseURI(baseUrl);
-        Response response = RestUtil.getResponse("/media/media-releases");
-        Document doc = Jsoup.parse(response.body().asString());
-        assertEquals(200, response.statusCode());
+                paraPage = "?page=" + i;
+            RestUtil.setBaseURI(baseUrl);
+            Response response = RestUtil.getResponse("/media/media-releases" + paraPage);
+            Document doc = Jsoup.parse(response.body().asString());
+            Elements links = doc.select(".views-row a");
+            List<String> titleLinks = links.eachAttr("href");
 
-        Elements links = doc.select(".views-row a");
-        List<String> titleLinks = links.eachAttr("href");
-
-        // for (int j = 0; j < titleLinks.size(); j++) {
-        Response article = RestUtil.getResponse(titleLinks.get(0));
-        System.out.println(article.body().asString());
-        String fileName = titleLinks.get(0).split("/")[2];
-        String content = article.body().asString();
-        try {
-            saveFile(content, fileName);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            for (int j = 0; j < titleLinks.size(); j++) {
+                count++;
+                Response article = RestUtil.getResponse(titleLinks.get(j));
+                String fileName = (i + 1) + "-" + count + "_" + titleLinks.get(j).split("/")[2];
+                String content = article.body().asString();
+                System.out.println("start to save file:" + fileName);
+                saveFile(content, fileName);
+            }
+            TimeUnit.MINUTES.sleep(timeInterval);
         }
     }
 
-    @Test
-    public void searchRepoShouldReturn200() {
-        RestUtil.setBaseURI("https://github.com/");
-        Response response = RestUtil.setAuth("lmaywy", "Passw0rd13$", "search/count?q=specflow&type=Code");
-        assertEquals(200, response.statusCode());
-        response.body().prettyPrint();
-    }
 
     public void saveFile(String content, String fileName)
             throws IOException {
-        String filePath="c:\\htmls\\";
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath+fileName + ".html"));
+        String filePath = "c:\\ACCC_Cases\\";
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath + fileName + ".html"));
         writer.write(content);
         writer.close();
     }
---
+++ src/test/java/com/baidu/infra/services/AppTestWithToken.java
@@ -0,0 +1,17 @@
+package com.baidu.infra.services;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+import utils.RestUtil;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTestWithToken {
+    @Test
+    public void searchRepoShouldReturn200() {
+        RestUtil.setBaseURI("https://github.com/");
+        Response response = RestUtil.setAuth("lmaywy", "Passw0rd13$", "search/count?q=specflow&type=Code");
+        assertEquals(200, response.statusCode());
+        response.body().prettyPrint();
+    }
+}
